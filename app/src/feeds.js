767470fd5dd113513a72e8a24e1bf70863e8d926
luke_gs <github@lukeskinner.email>
2015-02-16 21:53:22 +0000

    Show feeds in the db on the page.

--- app/src/feeds.js
+++ app/src/feeds.js
@@ -1,7 +1,19 @@
 export class Feeds{
+
+
   constructor(){
-alert("abc");
-    this.heading = 'Feedsa';
+    this.heading = 'Feeds';
     this.feeds = [];
+  }
+
+  activate(){
+var self = this;
+
+	return $.ajax({
+    	type: "GET", url: "/api/feeds",
+    	success: function (data) {
+        	self.feeds = data;
+    	}
+	});
   }
 }
