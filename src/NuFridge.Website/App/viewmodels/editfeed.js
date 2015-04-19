d7b3ea791336d38f3f1bb383ab4810d700d6355e
lukeskinner <fluffedup2009@gmail.com>
2015-04-19 06:13:45 +0100

    Reinstalled all NuGet packges in the website project & made some minor ui changes.

---
+++ src/NuFridge.Website/App/viewmodels/editfeed.js
@@ -0,0 +1,16 @@
+﻿define(function() {
+    var ctor = function () {
+        this.displayName = 'Edit Feed';
+    };
+
+    ctor.prototype.activate = function () {
+
+
+    }
+
+    ctor.prototype.compositionComplete = function () {
+
+    }
+
+    return ctor;
+});
--- src/NuFridge.Website/App/viewmodels/shell.js
+++ src/NuFridge.Website/App/viewmodels/shell.js
@@ -7,8 +7,8 @@
                 { route: 'feeds', moduleId: 'viewmodels/feeds', nav: true, icon: "mdi-navigation-apps" },
                 { route: 'profile', moduleId: 'viewmodels/profile', nav: true, icon: "mdi-action-account-box" },
                 { route: 'settings', moduleId: 'viewmodels/settings', nav: true, icon: "mdi-action-settings-applications" },
-                { route: 'feeds/:id', moduleId: 'viewmodels/addeditfeed', nav: false }, 
-                { route: 'feeds/create', moduleId: 'viewmodels/addeditfeed', nav: false }
+                { route: 'feeds/:id', moduleId: 'viewmodels/editfeed', nav: false }, 
+                { route: 'feeds/create', moduleId: 'viewmodels/addfeed', nav: false }
             ]).buildNavigationModel();
 
             router.on('router:navigation:complete').then(function() {
