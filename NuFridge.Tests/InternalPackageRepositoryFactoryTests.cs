40de37cdf9cb3577dedce3a7c7a35e91da8d93c0
lukeskinner <fluffedup2009@gmail.com>
2015-07-06 23:14:17 +0100

    Updated unit tests.

--- NuFridge.Tests/InternalPackageRepositoryFactoryTests.cs
+++ NuFridge.Tests/InternalPackageRepositoryFactoryTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Autofac;
 using Moq;
+using NuFridge.Shared.Server.Configuration;
 using NuFridge.Shared.Server.Modules;
 using NuFridge.Shared.Server.NuGet;
 using NUnit.Framework;
@@ -29,7 +30,7 @@ namespace NuFridge.Tests
         public void Setup()
         {
             var builder = new ContainerBuilder();
-
+            builder.RegisterInstance(new Mock<IHomeConfiguration>().Object);
             builder.RegisterModule(new NuGetModule());
             _container = builder.Build();
         }
