c12475519c833b38852f3b9571df34e5e17c45ec
lukeskinner <fluffedup2009@gmail.com>
2015-07-07 02:24:14 +0100

    Code for the manual creation of a batch response. It's not elegant and it doesn't handle exceptions (yet) but it works.

--- NuFridge.Shared/Server/Web/Actions/NuGetApiV2/BatchAction.cs
+++ NuFridge.Shared/Server/Web/Actions/NuGetApiV2/BatchAction.cs
@@ -1,45 +1,109 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using Nancy;
 using NuFridge.Shared.Server.Web.Batch;
+using NuGet;
 
 namespace NuFridge.Shared.Server.Web.Actions.NuGetApiV2
 {
     public class BatchAction : IAction
     {
-        public dynamic Execute(dynamic parameters, global::Nancy.INancyModule module)
+        private readonly INancyEngine _engine;
+        private readonly StringBuilder _responseBuilder;
+
+        public BatchAction(INancyEngine engine)
+        {
+            _engine = engine;
+            _responseBuilder = new StringBuilder();
+        }
+
+        public dynamic Execute(dynamic parameters, INancyModule module)
         {
             var contentType = module.Request.Headers["content-type"].First();
             var mimeType = contentType.Split(';').First();
             if (mimeType != "multipart/batch" && mimeType != "multipart/mixed")
                 return (int)HttpStatusCode.BadRequest;
 
-
-
             var multipartBoundry = Regex.Match(contentType, @"boundary=(?<token>[^\n\; ]*)").Groups["token"].Value.Replace("\"", "");
             if (string.IsNullOrEmpty(multipartBoundry))
                 return (int)HttpStatusCode.BadRequest;
 
+
+
             var multipart = new HttpMultipart(module.Request.Body, multipartBoundry);
+
+            IDictionary<string, IEnumerable<string>> headers = module.Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+
+            string batchResponseGuid = Guid.NewGuid().ToString().ToLower();
+
+            var response = new Response
+            {
+                ContentType = string.Format("{0}; boundary=batchresponse_{1}", mimeType, batchResponseGuid),
+                StatusCode = HttpStatusCode.Accepted
+            };
+
+            _responseBuilder.Append(string.Format("--batchresponse_{0}\r\n", batchResponseGuid));
+            _responseBuilder.Append("Content-Type: application/http\r\n");
+            _responseBuilder.Append("Content-Transfer-Encoding: binary\r\n\r\n");
 
             foreach (var boundry in multipart.GetBoundaries())
             {
                 using (var httpRequest = new StreamReader(boundry.Value))
                 {
-                    var requestStr = httpRequest.ReadToEnd();
-                    requestStr += "User-Agent: " + module.Request.Headers["User-Agent"].FirstOrDefault();
-                    requestStr += "\r" + "Host: " + module.Request.Url.HostName;
-                    requestStr += @"
-";
-                    var rawRequestStream = new MemoryStream(Encoding.UTF8.GetBytes(requestStr));
-                    var result = rawRequestStream.ReadAsRequest();
-                    var t = result;
+                    var httpRequestText = httpRequest.ReadToEnd();
+
+                    var rawRequestStream = new MemoryStream(Encoding.UTF8.GetBytes(httpRequestText));
+                    var nancyRequest = rawRequestStream.ReadAsRequest(headers);
+
+                    _engine.HandleRequest(nancyRequest, OnSuccess, OnError);
                 }
             }
 
-            return (int)HttpStatusCode.OK;
+            _responseBuilder.Append(string.Format("\r\n--batchresponse_{0}--", batchResponseGuid));
+
+            var responseText = _responseBuilder.ToString();
+            var byteData = Encoding.UTF8.GetBytes(responseText);
+
+            response.Headers.Add("Content-Length", byteData.Length.ToString());
+
+            response.Contents = contentStream =>
+            {
+                contentStream.Write(byteData, 0, byteData.Length);
+            };
+
+            return response;
+        }
+
+        private void OnSuccess(NancyContext obj)
+        {
+            _responseBuilder.Append("HTTP/1.1 200 OK\r\n");
+            _responseBuilder.Append(string.Format("Content-Type: {0}\r\n\r\n", obj.Response.ContentType));
+
+            using (var stream = new MemoryStream())
+            {
+                obj.Response.Contents(stream);
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                using (var sr = new StreamReader(stream))
+                {
+                    var responseText = sr.ReadToEnd();
+
+                    _responseBuilder.Append(responseText);
+                }
+            }
+        }
+
+        private void OnError(Exception exception)
+        {
+
         }
     }
 }
--- NuFridge.Shared/Server/Web/Batch/HttpParserStreamExtensions.cs
+++ NuFridge.Shared/Server/Web/Batch/HttpParserStreamExtensions.cs
@@ -18,16 +18,11 @@ namespace NuFridge.Shared.Server.Web.Batch
         /// <param name="scheme">The url scheme.</param>
         /// <param name="ip">The ip address.</param>
         /// <returns>Returns the parse <see cref="Request"/>.</returns>
-        public static Request ReadAsRequest(this Stream stream, string scheme = "http", string ip = null)
+        public static Request ReadAsRequest(this Stream stream, IDictionary<string, IEnumerable<string>> headers)
         {
             if (stream == null)
             {
                 throw new ArgumentNullException("stream");
-            }
-
-            if (string.IsNullOrWhiteSpace(scheme))
-            {
-                throw new ArgumentNullException("scheme");
             }
 
             var requestLine = RequestLineParser.Parse(stream);
@@ -50,10 +45,10 @@ namespace NuFridge.Shared.Server.Web.Batch
                 var splitUri = requestLine.Uri.Split('?');
                 url.Path = splitUri[0];
                 url.Query = splitUri.Length == 2 ? splitUri[1] : string.Empty;
-                url.Scheme = scheme;
+                url.Scheme = "https";
             }
 
-            var headers = HeaderParser.Parse(stream);
+
             IEnumerable<string> headerValues;
             if (headers.TryGetValue("Host", out headerValues))
             {
@@ -62,7 +57,7 @@ namespace NuFridge.Shared.Server.Web.Batch
 
             var nestedRequestStream = new RequestStream(new HttpMultipartSubStream(stream, stream.Position, stream.Length), stream.Length - stream.Position, true);
 
-            var request = new Request(requestLine.Method, url, nestedRequestStream, headers, ip);
+            var request = new Request(requestLine.Method, url, nestedRequestStream, headers, null);
 
             return request;
         }
