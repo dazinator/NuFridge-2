c2ac6524c6e84f93642cbde9932ff901818d5bb9
lukeskinner <github@lukeskinner.email>
2015-07-16 08:45:56 +0100

    Start of work for SMTP server settings. Added basic UI.

---
+++ NuFridge.Website/App/forms/smtpconfiguration.js
@@ -0,0 +1,30 @@
+﻿define(['plugins/router', 'api', 'auth', 'databinding-feed'], function (router, api, auth, databindingFeed) {
+    var ctor = function () {
+        var self = this;
+        self.isSaving = ko.observable(false);
+        self.isCancelNavigating = ko.observable(false);
+    };
+
+    ctor.prototype.activate = function (activationData) {
+        var self = this;
+
+    }
+
+
+    ctor.prototype.compositionComplete = function () {
+        var self = this;
+
+    };
+
+    ctor.prototype.submitClick = function() {
+        var self = this;
+    };
+
+    ctor.prototype.cancelClick = function() {
+        var self = this;
+
+    };
+
+
+    return ctor;
+});
