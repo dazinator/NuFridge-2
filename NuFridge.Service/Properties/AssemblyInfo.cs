b023ee3f418e8fbf6676ee3c9ddad972e8059204
lukeskinner <fluffedup2009@gmail.com>
2015-07-16 19:51:57 +0100

    Updated assembly info.

--- NuFridge.Service/Properties/AssemblyInfo.cs
+++ NuFridge.Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuFridge.Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NuFridge Service")]
+[assembly: AssemblyDescription("The service which runs the NuFridge NuGet feeds.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NuFridge.Test")]
+[assembly: AssemblyCompany("NuFridge")]
+[assembly: AssemblyProduct("NuFridge Service")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
