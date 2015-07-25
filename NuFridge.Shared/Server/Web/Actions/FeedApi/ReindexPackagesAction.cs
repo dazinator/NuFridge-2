5185c73e0e0937ea1b9fec120e292a749ac7679d
lukeskinner <fluffedup2009@gmail.com>
2015-07-25 19:24:10 +0100

    Fixes #93. Added server side code to check if a feed is already reindexing its packages or pending a reindex. Done via querying hangfire for a list of parameters associated with the job.

--- NuFridge.Shared/Server/Web/Actions/FeedApi/ReindexPackagesAction.cs
+++ NuFridge.Shared/Server/Web/Actions/FeedApi/ReindexPackagesAction.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Hangfire;
 using Nancy;
+using Nancy.Responses;
 using Nancy.Security;
+using NuFridge.Shared.Logging;
 using NuFridge.Shared.Server.Scheduler.Jobs;
 
 namespace NuFridge.Shared.Server.Web.Actions.FeedApi
@@ -13,6 +15,7 @@ namespace NuFridge.Shared.Server.Web.Actions.FeedApi
     public class ReindexPackagesAction : IAction
     {
         private readonly ReindexPackagesForFeedJob _reIndexJob;
+        private readonly ILog _log = LogProvider.For<ReindexPackagesAction>();
 
         public ReindexPackagesAction(ReindexPackagesForFeedJob reIndexJob)
         {
@@ -25,9 +28,65 @@ namespace NuFridge.Shared.Server.Web.Actions.FeedApi
 
             int feedId = parameters.id;
 
+            var monitoringApi = JobStorage.Current.GetMonitoringApi();
+
+            var processingJobs = monitoringApi.ProcessingJobs(0, int.MaxValue);
+
+            foreach (var processingJob in processingJobs)
+            {
+                if (processingJob.Value.Job.Type == typeof (ReindexPackagesForFeedJob))
+                {
+                    //TODO Make a unit test for this
+                    if (processingJob.Value.Job.Arguments.Count() != 2)
+                    {
+                        _log.Error("The reindex packages job has an unexpected number of arguments.");
+                        throw new InvalidOperationException("The reindex packages job has an unexpected number of arguments.");
+                    }
+                    var strFeedIdForJob = processingJob.Value.Job.Arguments[1];
+                    int feedIdForJob;
+                    if (!int.TryParse(strFeedIdForJob, out feedIdForJob))
+                    {
+                        _log.Error("The reindex packages action could not read the feed id.");
+                        throw new FormatException("The reindex packages action could not read the feed id.");
+                    }
+
+                    if (feedIdForJob == feedId)
+                    {
+                        return new TextResponse(HttpStatusCode.Conflict, "The requested feed is already reindexing its packages.");
+                    }
+                }
+            }
+
+            var enqueuedJobs = monitoringApi.EnqueuedJobs("default", 0, int.MaxValue);
+
+            foreach (var enqueuedJob in enqueuedJobs)
+            {
+                if (enqueuedJob.Value.Job.Type == typeof(ReindexPackagesForFeedJob))
+                {
+                    //TODO Make a unit test for this
+                    if (enqueuedJob.Value.Job.Arguments.Count() != 2)
+                    {
+                        _log.Error("The reindex packages job has an unexpected number of arguments.");
+                        throw new InvalidOperationException("The reindex packages job has an unexpected number of arguments.");
+                    }
+                    var strFeedIdForJob = enqueuedJob.Value.Job.Arguments[1];
+                    int feedIdForJob;
+                    if (!int.TryParse(strFeedIdForJob, out feedIdForJob))
+                    {
+                        _log.Error("The reindex packages action could not read the feed id.");
+                        throw new FormatException("The reindex packages action could not read the feed id.");
+                    }
+
+                    if (feedIdForJob == feedId)
+                    {
+                        return new TextResponse(HttpStatusCode.Conflict, "The requested feed is already pending a package reindex.");
+                    }
+                }
+            }
+
             BackgroundJob.Enqueue(() => _reIndexJob.Execute(JobCancellationToken.Null, feedId));
 
-            return new Response {StatusCode = HttpStatusCode.OK};
+            return new TextResponse(HttpStatusCode.OK, "The requested feed will be reindexed.");
         }
     }
 }
