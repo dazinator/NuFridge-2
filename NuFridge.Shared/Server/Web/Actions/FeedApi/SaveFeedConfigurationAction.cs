f0abe5f46cb73385538250af71233f76a830b5cf
lukeskinner <fluffedup2009@gmail.com>
2015-07-27 21:48:05 +0100

    Fixes #33. Configurable feed folder.

--- NuFridge.Shared/Server/Web/Actions/FeedApi/SaveFeedConfigurationAction.cs
+++ NuFridge.Shared/Server/Web/Actions/FeedApi/SaveFeedConfigurationAction.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using Nancy;
 using Nancy.ModelBinding;
+using Nancy.Responses;
 using Nancy.Security;
 using NuFridge.Shared.Logging;
 using NuFridge.Shared.Model;
@@ -36,13 +39,55 @@ namespace NuFridge.Shared.Server.Web.Actions.FeedApi
                     return HttpStatusCode.BadRequest;
                 }
 
+                if (string.IsNullOrWhiteSpace(feedConfig.Directory))
+                {
+                    return new TextResponse(HttpStatusCode.BadRequest, "A feed directory must be provided.");
+                }
+
                 ITransaction transaction = _store.BeginTransaction();
 
-                var existingFeedExists = transaction.Query<IFeedConfiguration>().Where("FeedId = @feedId").Parameter("feedId", feedId).Count() > 0;
+                var existingFeedConfig = transaction.Query<IFeedConfiguration>().Where("FeedId = @feedId").Parameter("feedId", feedId).First();
 
-                if (!existingFeedExists)
+                if (existingFeedConfig == null)
                 {
                     return HttpStatusCode.NotFound;
+                }
+
+                if (feedConfig.Directory != existingFeedConfig.Directory)
+                {
+                    var directoryUsedByOtherConfigs = transaction.Query<IFeedConfiguration>()
+                        .Where("Directory = @directory")
+                        .Parameter("directory", feedConfig.Directory).ToList();
+
+                    if (directoryUsedByOtherConfigs.Any(dr => dr.Id != feedConfig.Id))
+                    {
+                        return new TextResponse(HttpStatusCode.BadRequest, "The feed directory is already being used for another feed.");
+                    }
+
+                    _log.Info("Changing the directory from " + existingFeedConfig.Directory + " to " + feedConfig.Directory + " for feed " + feedId);
+
+                    if (Directory.Exists(existingFeedConfig.Directory))
+                    {
+                        if (!Directory.Exists(feedConfig.Directory))
+                        {
+                            _log.Debug("Creating the " + feedConfig.Directory + " folder for feed id " + feedId);
+                            Directory.CreateDirectory(feedConfig.Directory);
+                        }
+
+                        try
+                        {
+                            MoveDirectory(existingFeedConfig.Directory, feedConfig.Directory);
+                        }
+                        catch (Exception ex)
+                        {
+                            _log.ErrorException("There was an error moving the " + existingFeedConfig.Directory + " folder to " + feedConfig.Directory + " for feed id " + feedId + ". The directory has not been updated for the feed but packages may have been moved. Error: " + ex.Message, ex);
+                            return new TextResponse("There was an error moving the " + existingFeedConfig.Directory + " folder to " + feedConfig.Directory + " for feed id " + feedId + ". The directory has not been updated for the feed but packages may have been moved. Error: " + ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        _log.Info("No existing feed directory exists for " + feedId + ", so no packages will be moved.");
+                    }
                 }
 
                 transaction.Update(feedConfig);
@@ -58,6 +103,26 @@ namespace NuFridge.Shared.Server.Web.Actions.FeedApi
 
 
             return feedConfig;
+        }
+
+        public static void MoveDirectory(string source, string target)
+        {
+            var sourcePath = source.TrimEnd('\\', ' ');
+            var targetPath = target.TrimEnd('\\', ' ');
+            var files = Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                                 .GroupBy(s => Path.GetDirectoryName(s));
+            foreach (var folder in files)
+            {
+                var targetFolder = folder.Key.Replace(sourcePath, targetPath);
+                Directory.CreateDirectory(targetFolder);
+                foreach (var file in folder)
+                {
+                    var targetFile = Path.Combine(targetFolder, Path.GetFileName(file));
+                    if (File.Exists(targetFile)) File.Delete(targetFile);
+                    File.Move(file, targetFile);
+                }
+            }
+            Directory.Delete(source, true);
         }
     }
 }
--- NuFridge.Website/App/viewmodels/databinding/feedconfig.js
+++ NuFridge.Website/App/viewmodels/databinding/feedconfig.js
@@ -6,6 +6,8 @@
             FeedId: ko.observable(0),
             Id: ko.observable(0),
             PackagesDirectory: ko.observable(""),
+            SymbolsDirectory: ko.observable(""),
+            Directory: ko.observable(""),
             RetentionPolicyEnabled: ko.observable(false),
             MaxPrereleasePackages: ko.observable(0).extend({ required: true }),
             MaxReleasePackages: ko.observable(0).extend({ required: true }),
