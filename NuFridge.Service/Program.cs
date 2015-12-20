e8ee17dc67154e4c570e5c2c9346c2f69be8d535
lukeskinner <fluffedup2009@gmail.com>
2015-12-20 22:08:33 +0000

    If there is an error and the application is running as a console app, prompt the user to close the application so they can at least view the error.

--- NuFridge.Service/Program.cs
+++ NuFridge.Service/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace NuFridge.Service
+﻿using System;
+
+namespace NuFridge.Service
 {
     class Program
     {
         static int Main(string[] args)
         {
             var code = Startup.Run(args);
+            if (Environment.UserInteractive)
+            {
+                if (code > 0)
+                {
+
+                    Console.WriteLine();
+                    Console.WriteLine("NuFridge has encountered an error. Press any key to exit.");
+                    Console.Read();
+                }
+            }
             return code;
         }
     }
