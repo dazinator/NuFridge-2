8013823be1e0d206885373421f3dcd77bb628d4f
lukeskinner <fluffedup2009@gmail.com>
2015-12-21 21:01:07 +0000

    Fix for a change in aurelia-auth which means I need to include a # before my redirect routes.

--- NuFridge.Website.New/src/authConfig.js
+++ NuFridge.Website.New/src/authConfig.js
@@ -4,14 +4,14 @@ var config = {
             clientId: '503113912592-283tcms15a2mh5ksggtv60rmt7afnf3f.apps.googleusercontent.com'
         }
     },
-    loginRedirect: "dashboard",
+    loginRedirect: "#dashboard",
     baseUrl: "",
     loginUrl: "api/signin",
-    loginRoute: 'signin',
+    loginRoute: '#signin',
     signupRedirect: "",
     httpInterceptor: true,
     authToken: "Token",
-    logoutRedirect: "signin",
+    logoutRedirect: "#signin",
     profileUrl: "api/account"
 };
 
